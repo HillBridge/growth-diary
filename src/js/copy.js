3ebb5797bc34f1072d7248edf01aaf98a111a7e8
乔国胜 <294238921@qq.com>
2023-12-15 19:17:33 +0800

    js 数组对象签拷贝

---
+++ src/js/copy.js
@@ -0,0 +1,24 @@
+// js 拷贝
+// 浅拷贝数组 slice (指的是copy数组的第一层, copy后的数据更改第一层原数据结构不改变, 深层的数据将改变)
+const arr = [1, 2, 3];
+function copy1(arr) {
+  return arr.slice();
+}
+
+// console.log("浅拷贝数组", copy1(arr));
+
+// 浅拷贝对象
+const obj = {
+  name: "foo",
+  age: 12,
+  nums: [{ count: 1 }],
+};
+function copy2(obj) {
+  return Object.assign({}, obj);
+}
+const res2 = copy2(obj);
+// console.log("浅拷贝对象", copy2(obj));
+res2.nums[0].count = 2;
+res2.age = 14;
+
+// console.log("改变了值", obj);
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
-import "./js/higherOrderFunction";
+import "./js/copy";
 
 async function bootstrapWorker() {
   const { worker } = await import("./mocks");
